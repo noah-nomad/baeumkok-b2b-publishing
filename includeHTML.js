c4cca7f707a861c61cf9346ca8368f5399b0b15a
nomad-jane <jane@noah-nomad.com>
2022-09-13 16:19:43 +0900

    feat: 220913 테스트

---
+++ includeHTML.js
@@ -0,0 +1,18 @@
+// load HTML
+function loadHTML(file)
+{
+    const rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                const allText = rawFile.responseText;
+                document.write(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
